f14dee1ec193a6d7702026227dab67495df55c1f
123qwerty12345 <132197172+123qwerty12345@users.noreply.github.com>
2023-05-02 18:19:06 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Ponchik hub",
-   LoadingTitle = "Ponchik hub",
-   LoadingSubtitle = "Ponchik hub",
+   Name = "Test",
+   LoadingTitle = "Testing",
+   LoadingSubtitle = "Tested",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = hub, -- Create a custom folder for your hub/game
@@ -18,7 +18,7 @@ local Window = Rayfield:CreateWindow({
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
+      Note = "Join discrod to get key",
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
