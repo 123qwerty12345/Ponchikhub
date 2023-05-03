2e046e6a49bc4b0e2d513817f75917a64aa3e4e6
123qwerty12345 <132197172+123qwerty12345@users.noreply.github.com>
2023-05-03 12:09:21 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -22,7 +22,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Pon"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
@@ -37,4 +37,14 @@ local Button = Tab:CreateButton({
    end,
 })
 
-
+local Slider = Tab:CreateSlider({
+   Name = "Speed",
+   Range = {0, 100},
+   Increment = 10,
+   Suffix = "SpeedHack",
+   CurrentValue = 10,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+   end,
+})
