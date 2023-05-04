03554fe0da6c6a023cb94f8b2834f4d3f5160191
123qwerty12345 <132197172+123qwerty12345@users.noreply.github.com>
2023-05-04 21:01:28 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -18,17 +18,17 @@ local Window = Rayfield:CreateWindow({
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "Join discrod to get key",
+      Note = "не крякать!",
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Pon"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"key"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
 local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
-local Section = Tab:CreateSection("Section Example")
-Section:Set("Section Example")
+local Section = Tab:CreateSection("Player")
+Section:Set("Player")
 
 local Button = Tab:CreateButton({
    Name = "Fly(e)",
@@ -44,7 +44,38 @@ local Slider = Tab:CreateSlider({
    Suffix = "SpeedHack",
    CurrentValue = 10,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
+   Callback = function(s)
    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+   end,
+})
+
+
+local Slider = Tab:CreateSlider({
+   Name = "Jump",
+   Range = {0, 100},
+   Increment = 10,
+   Suffix = "Jumppower",
+   CurrentValue = 10,
+   Flag = "Slider2",
+   Callback = function(s)
+   game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+   end,
+})
+
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+local Section = Tab:CreateSection("Arsenal")
+Section:Set("Arsenal")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Toggle Example",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+for i,v in pairs(game:GetDescendants()) do
+if v.Name == 'DeadHP' then
+v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+wait(0.5)
+end
+end
    end,
 })
